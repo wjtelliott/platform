b76db4e24a59c393251ac92a9b4e6eb62b39bd65
unknown <wjtelliott@gmail.com>
2021-12-21 18:57:05 -0600

    Attempting to fix githubIO page crash

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,23 @@
 // Some of these scripts will have to load before the next.
 // This list has priority to load the last first, and needs to be in that order.
 let scriptsList = [
-    './scripts/player.js',
-    './scripts/map.js',
-    './scripts/tile.js',
-    './scripts/render.js',
-    './scripts/entity.js',
-    './scripts/gutil.js',
-    './scripts/gtypes.js',
+
+    /*
+
+        We unfortunately have to use a direct address here, otherwise
+        github.io will NOT load correctly and will crash our page.
+
+        Tested, and using these addresses works locally, as well as ./scripts/...js
+
+     */
+
+    'https://wjtelliott.github.io/platform/scripts/player.js',
+    'https://wjtelliott.github.io/platform/scripts/map.js',
+    'https://wjtelliott.github.io/platform/scripts/tile.js',
+    'https://wjtelliott.github.io/platform/scripts/render.js',
+    'https://wjtelliott.github.io/platform/scripts/entity.js',
+    'https://wjtelliott.github.io/platform/scripts/gutil.js',
+    'https://wjtelliott.github.io/platform/scripts/gtypes.js',
 ];
 const loadScripts = function()
 {
@@ -21,7 +31,7 @@ function runJScriptFile(path)
 
     // Make sure we declare this before appending it to body
     script.onload = function() { loadScripts(); };
-    document.body.append(script);
+    document.getElementsByTagName("head")[0].appendChild(script);
 }
 
 
